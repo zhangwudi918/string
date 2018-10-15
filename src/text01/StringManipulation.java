ddd1602e4582b7849d3682957590387db7355cbc
“zhangwudi918 <1106157813@qq.com>
2018-10-15 22:12:34 +0800

    eeee

---
+++ src/text01/StringManipulation.java
@@ -0,0 +1,39 @@
+package text01;
+
+import java.util.Scanner;
+
+public class StringManipulation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int index;
+        boolean end = false;
+        boolean start = false;
+        StringBuilder sb = new StringBuilder(sc.nextLine());
+
+        while (true) {
+            index = sb.indexOf("*");
+            if (index == sb.length() - 1) {
+                end = true;
+
+            }
+            if (index == 0) {
+                start = true;
+            }
+            if (index >= 0) {
+                sb = sb.deleteCharAt(index);
+            }
+            if (index == -1) {
+                break;
+            }
+        }
+        if (start == true) {
+            sb.insert(0, "*");
+        }
+        if (end == true) {
+            sb.append("*");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
