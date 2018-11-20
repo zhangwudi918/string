83270d6686758b90ef9c7e0e96db33a92a307689
“zhangwudi918 <1106157813@qq.com>
2018-11-20 22:07:19 +0800

    改了map查询方式  存入键值对属性换了一下

--- src/webserver/SaveMap.java
+++ src/webserver/SaveMap.java
@@ -12,26 +12,32 @@ public class SaveMap {
 	public static synchronized String savemessage(String message) {
 		System.out.println("开始查找表格");
 		// 遍历集合，判断键值对中value的值和这次客户端输入的信息是否相同
-		Set<Entry<String, String>> set = lexicon.entrySet();
+		boolean check = lexicon.containsKey(message);
+		if (check) {
+			return "该词已存在，序号为：" + lexicon.get(message);
+		} else {
+
+
+	/*	Set<Entry<String, String>> set = lexicon.entrySet();
 		for (Entry<String, String> e : set) {
 			if (e.getValue().equals(message)) {
 
 				return "该词已存在，序号为：" + e.getKey();
 				// 若存在，程序到此结束
 			}
-		}
-		// 若不存在，根据集合内键值对数量生成key值，然后存入集合
-		int len = lexicon.size();
-		StringBuilder sb = new StringBuilder();
-		while (len / 62 >= 1) {
-			sb.append(table[len % 62]);
-			len = len / 62;
+		}*/
+			// 若不存在，根据集合内键值对数量生成key值，然后存入集合
+			int len = lexicon.size();
+			StringBuilder sb = new StringBuilder();
+			while (len / 62 >= 1) {
+				sb.append(table[len % 62]);
+				len = len / 62;
+
+			}
+			String value = sb.append(table[len % 62]).reverse().toString();
+			lexicon.put(message, value);
+			return "提交成功! 序号为：" + value;
 
 		}
-		String key = sb.append(table[len % 62]).reverse().toString();
-		lexicon.put(key, message);
-		return "提交成功! 序号为：" + key;
-
 	}
-
 }
